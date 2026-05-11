0476ccdc9c60a893ee6465833bc7a63a91df4dc3
Herr-Pollt <enzoyang990521@gmail.com>
2026-05-11 16:45:48 -0400

    Succesful plotting with motion and pauses.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-
+n = 100#number of steps
 x = np.zeros(100)
 y = np.zeros(100)
 temp = 0
+plt.figure(0)
 
-for i in range(100):
+for i in range(n):
     temp = random.randint(1, 4)
     if temp == 1:
         if i == 0:
@@ -33,12 +34,12 @@ for i in range(100):
             x[i] = x[i-1] - 1
             y[i] = y[i-1]
 
-    plt.figure()
-    plt.clf
+    plt.clf()
     plt.subplot()
-    plt.plot(x[:i], "b-o")
+    plt.plot(x[:i], "b")
     plt.subplot()
-    plt.plot(y[:i], "r-o")
+    plt.plot(y[:i], "r")
     plt.title("Blue is X, Red is Y")
-    plt.show()
     plt.pause(0.1)
+
+plt.show()
