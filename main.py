45f7398d0d3ded753590dc3f3e55f0a67266e4e0
Herr-Pollt <enzoyang990521@gmail.com>
2026-05-11 16:50:11 -0400

    deleted unneccessary components

--- main.py
+++ main.py
@@ -22,29 +22,17 @@ plt.figure(0)
 for i in range(n):
     temp = random.randint(1, 4)
     if temp == 1:
-        if i == 0:
-            y[i] = 1
-        else:
-            y[i] = y[i-1] + 1
-            x[i] = x[i-1]
+        y[i] = y[i-1] + 1
+        x[i] = x[i-1]
     elif temp == 2:
-        if i == 0:
-            x[i] = 1
-        else:
-            x[i] = x[i-1] + 1
-            y[i] = y[i-1]
+        x[i] = x[i-1] + 1
+        y[i] = y[i-1]
     elif temp == 3:
-        if i == 0:
-            y[i] = -1
-        else:
-            y[i] = y[i-1] - 1
-            x[i] = x[i-1]
+        y[i] = y[i-1] - 1
+        x[i] = x[i-1]
     elif temp == 4:
-        if i == 0:
-            x[i] = -1
-        else:
-            x[i] = x[i-1] - 1
-            y[i] = y[i-1]
+        x[i] = x[i-1] - 1
+        y[i] = y[i-1]
 
     plt.clf()
     plt.subplot()
